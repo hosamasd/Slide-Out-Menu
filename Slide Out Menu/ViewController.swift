2d5249d105431b787d774f612c87b27b6dbf885c
hosam <hosam@Zigzag.local>
2019-05-18 16:13:14 +0200

    Initial Commit

---
+++ Slide Out Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slide Out Menu
+//
+//  Created by hosam on 5/18/19.
+//  Copyright © 2019 hosam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
